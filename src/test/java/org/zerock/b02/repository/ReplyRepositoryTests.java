825086b4ddfef34956e2dd692c0f2fa05221ccf1
JBKIM <drv98@naver.com>
2024-07-10 15:33:20 +0900

    댓글 테스트

---
+++ src/test/java/org/zerock/b02/repository/ReplyRepositoryTests.java
@@ -0,0 +1,34 @@
+package org.zerock.b02.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.b02.domain.Board;
+import org.zerock.b02.domain.Reply;
+
+@SpringBootTest
+@Log4j2
+public class ReplyRepositoryTests {
+
+    @Autowired
+    private ReplyRepository replyRepository;
+
+    @Test
+    public void testSave() {
+
+        //DB에 있는 게시글
+        Long bno = 100L;
+
+        Board board = Board.builder().bno(bno).build();
+
+        Reply reply = Reply.builder()
+                .board(board)
+                .replyText("댓글2....")
+                .replyer("replyer2")
+                .build();
+
+        Reply result = replyRepository.save(reply);
+        log.info(result);
+    }
+}
