4427f0c4b0bea242d5a469a2fcfb2edc4e323ffd
JBKIM <drv98@naver.com>
2024-07-10 14:41:44 +0900

    댓글 달기 유효성 테스트

--- src/main/java/org/zerock/b02/controller/ReplyController.java
+++ src/main/java/org/zerock/b02/controller/ReplyController.java
@@ -1,8 +1,11 @@
 package org.zerock.b02.controller;
 
+import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +21,16 @@ import java.util.Map;
 public class ReplyController {
 
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String,Long>> register(@RequestBody ReplyDTO replyDTO) {
+    public ResponseEntity<Map<String,Long>> register(@Valid @RequestBody ReplyDTO replyDTO,
+                                                     BindingResult bindingResult) throws BindException {
         log.info(replyDTO);
-        Map<String,Long> map = Map.of("rno", 111L);
-        return ResponseEntity.ok(map); //ResponseEntity 는 상태코드와 함께 객체 전달
+        if(bindingResult.hasErrors()) {
+            //유효성검사 에러시 바인드예외를 발생시킴
+            throw new BindException(bindingResult);
+        }
+
+        Map<String,Long> resultMap = Map.of("rno", 111L);
+
+        return ResponseEntity.ok(resultMap); //ResponseEntity 는 상태코드와 함께 객체 전달
     }
 }
