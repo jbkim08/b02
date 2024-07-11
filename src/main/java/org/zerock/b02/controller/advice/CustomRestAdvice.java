8ab579ab25048433f410deadfd014c5c0dde6387
JBKIM <drv98@naver.com>
2024-07-11 14:16:37 +0900

    게시글번호가 없을때 예외처리

--- src/main/java/org/zerock/b02/controller/advice/CustomRestAdvice.java
+++ src/main/java/org/zerock/b02/controller/advice/CustomRestAdvice.java
@@ -1,10 +1,12 @@
 package org.zerock.b02.controller.advice;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -29,6 +31,16 @@ public class CustomRestAdvice {
                 errorMap.put(fieldError.getField(), fieldError.getCode());
             });
         }
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleFKException(Exception e) {
+        log.error(e);
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("time", ""+System.currentTimeMillis());
+        errorMap.put("msg", "FK violation");
         return ResponseEntity.badRequest().body(errorMap);
     }
 }
