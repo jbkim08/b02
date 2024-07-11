af051643586e1f9334670bbd552518c38fc94d56
JBKIM <drv98@naver.com>
2024-07-11 14:10:18 +0900

    댓글 등록 컨트롤러

--- src/main/java/org/zerock/b02/controller/ReplyController.java
+++ src/main/java/org/zerock/b02/controller/ReplyController.java
@@ -1,13 +1,18 @@
 package org.zerock.b02.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.b02.dto.ReplyDTO;
+import org.zerock.b02.service.ReplyService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,12 +20,21 @@ import java.util.Map;
 @RestController
 @RequestMapping("/replies")
 @Log4j2
+@RequiredArgsConstructor
 public class ReplyController {
 
+    private final ReplyService replyService;
+
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String,Long>> register(@RequestBody ReplyDTO replyDTO) {
-        log.info(replyDTO);
-        Map<String,Long> map = Map.of("rno", 111L);
+    public ResponseEntity<Map<String,Long>> register(@Valid @RequestBody ReplyDTO replyDTO,
+                                                     BindingResult bindingResult) throws BindException {
+        log.info("Registering new reply: " + replyDTO);
+        if (bindingResult.hasErrors()) {
+            throw new BindException(bindingResult);
+        }
+        Map<String,Long> map = new HashMap<>();
+        Long rno = replyService.register(replyDTO);
+        map.put("rno", rno);
         return ResponseEntity.ok(map); //ResponseEntity 는 상태코드와 함께 객체 전달
     }
 }
