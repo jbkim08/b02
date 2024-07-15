580c0fa27760928de7218940fe5e833cc976ba5b
JBKIM <drv98@naver.com>
2024-07-15 09:22:43 +0900

    업로드 파일 설정

---
+++ src/main/java/org/zerock/b02/dto/UploadFileDTO.java
@@ -0,0 +1,12 @@
+package org.zerock.b02.dto;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Data
+public class UploadFileDTO {
+    //여러개 파일 리스트
+    private List<MultipartFile> files;
+}
