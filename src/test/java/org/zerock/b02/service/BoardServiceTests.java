5d8047dfb32ff8402f160290390e3a7c836f6e84
JBKIM <drv98@naver.com>
2024-07-16 10:38:03 +0900

    게시물 등록(이미지와 함께) 테스트

--- src/test/java/org/zerock/b02/service/BoardServiceTests.java
+++ src/test/java/org/zerock/b02/service/BoardServiceTests.java
@@ -9,6 +9,9 @@ import org.zerock.b02.dto.PageRequestDTO;
 import org.zerock.b02.dto.PageResponseDTO;
 import org.zerock.b02.service.BoardService;
 
+import java.util.Arrays;
+import java.util.UUID;
+
 @SpringBootTest
 @Log4j2
 public class BoardServiceTests {
@@ -56,6 +59,27 @@ public class BoardServiceTests {
 
         PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
         log.info(responseDTO);
+    }
+
+    @Test
+    public void testRegisterWithImages(){
+        log.info(boardService.getClass().getName());
+
+        BoardDTO boardDTO = BoardDTO.builder()
+                .title("샘플 제목....")
+                .content("샘플 내용...")
+                .writer("user00")
+                .build();
+
+        boardDTO.setFileNames(
+                Arrays.asList(
+                        UUID.randomUUID()+"_aaa.jpg",
+                        UUID.randomUUID()+"_bbb.jpg",
+                        UUID.randomUUID()+"_ccc.jpg"
+                )
+        );
+        Long bno = boardService.register(boardDTO);
+        log.info("bno: " + bno);
     }
 
 }
