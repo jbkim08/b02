0271b22163ea3773ad4a3f6bbb49b1c2f6386f23
JBKIM <drv98@naver.com>
2024-07-16 10:44:07 +0900

    엔티티 => 보드로 변환

--- src/main/java/org/zerock/b02/service/BoardService.java
+++ src/main/java/org/zerock/b02/service/BoardService.java
@@ -3,6 +3,9 @@ package org.zerock.b02.service;
 import org.zerock.b02.domain.Board;
 import org.zerock.b02.dto.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public interface BoardService {
     //새 글을 등록
     Long register(BoardDTO boardDTO);
@@ -39,5 +42,26 @@ public interface BoardService {
         }
         return board;
     }
+
+    default BoardDTO entityToDto(Board board) {
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(board.getBno())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .writer(board.getWriter())
+                .regDate(board.getRegDate())
+                .modDate(board.getModDate())
+                .build();
+
+        List<String> fileNames =
+            board.getImageSet().stream().sorted()
+                    .map(boardImage -> boardImage.getUuid()+"_"+boardImage.getFileName())
+                    .collect(Collectors.toList());
+
+        boardDTO.setFileNames(fileNames);
+
+        return boardDTO;
+    }
+
 }
 
