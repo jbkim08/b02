1548525425008d6c7fe4e2a9bbbfcaa18673f5ab
JBKIM <drv98@naver.com>
2024-07-16 14:10:27 +0900

    upload.js 추가 및 모달창 추가

---
+++ src/main/resources/static/js/upload.js
@@ -0,0 +1,20 @@
+async function uploadToServer (formObj){
+    console.log("업로드 to 서버....")
+    console.log(formObj)
+
+    const response = await axios({
+        method: 'post',
+        url: '/upload',
+        data: formObj,
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })
+
+    return response.data
+}
+
+async function removeFileToServer(uuid, fileName) {
+    const response = await axios.delete(`/remove/${uuid}_${fileName}`)
+    return response.data
+}
