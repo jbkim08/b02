5e146d49ecf12dd8ccbe2af2266087130ff6e0a3
JBKIM <drv98@naver.com>
2024-07-16 09:35:08 +0900

    @BatchSize 로 최적화

--- src/main/java/org/zerock/b02/domain/Board.java
+++ src/main/java/org/zerock/b02/domain/Board.java
@@ -2,6 +2,7 @@ package org.zerock.b02.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -33,6 +34,7 @@ public class Board extends BaseEntity {
             fetch = FetchType.LAZY,
             orphanRemoval = true )
     @Builder.Default
+    @BatchSize(size = 20)
     private Set<BoardImage> imageSet = new HashSet<>(); //중복안되는 이미지객체 리스트
 
     public void change(String title, String content) {
