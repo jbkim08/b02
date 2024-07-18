3c79414c08ed31f6a728c34537e2b295a2506282
JBKIM <drv98@naver.com>
2024-07-18 10:29:25 +0900

    어노테이션 인증

--- src/main/java/org/zerock/b02/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/b02/config/CustomSecurityConfig.java
@@ -1,26 +1,33 @@
 package org.zerock.b02.config;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @Log4j2
-@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)  //메소드에 @으로 시큐리티 설정
 public class CustomSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); //패스워드 암호화 객체 등록
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         log.info("----------------configure-----------------");
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(formLogin -> formLogin.loginPage("/login"));
+                .formLogin(formLogin -> {});
 
         return http.build();
     }
---
+++ src/main/java/org/zerock/b02/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package org.zerock.b02.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    //생성자 주입으로 등록된 암호화객체를 주입
+    private final PasswordEncoder passwordEncoder;
+    //시큐리티 인증에서 로그인하는 메소드
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("loadUserByUsername : " + username);
+        //메모리 인증
+        UserDetails userDetails = User.builder()
+                .username("user1")
+                .password(passwordEncoder.encode("1234"))
+                .authorities("ROLE_USER")
+                .build();
+        return userDetails;
+    }
+}
