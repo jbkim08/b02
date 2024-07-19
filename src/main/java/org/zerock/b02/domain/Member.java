7f94be97f4da39d9d08b5fca4520de2a93503bff
JBKIM <drv98@naver.com>
2024-07-19 11:23:55 +0900

    멤버(유저) 엔티티와 권한 Role

---
+++ src/main/java/org/zerock/b02/domain/Member.java
@@ -0,0 +1,50 @@
+package org.zerock.b02.domain;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "roleSet")
+public class Member extends BaseEntity {
+
+    @Id
+    private String mid;
+    private String mpw;
+    private String email;
+    private String del; //탈퇴
+
+    private boolean social; //쇼셜로 로그인
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @Builder.Default
+    private Set<MemberRole> roleSet;
+
+    public  void changePassword(String password) {
+        this.mpw = password;
+    }
+    public void changeEmail(String email) {
+        this.email = email;
+    }
+    public void changeDel(String del) {
+        this.del = del;
+    }
+    public void addRole(MemberRole role) {
+        this.roleSet.add(role);
+    }
+    public void clearRoles(){
+        this.roleSet.clear();
+    }
+    public void changeSocial(boolean social) {
+        this.social = social;
+    }
+}
---
+++ src/main/java/org/zerock/b02/domain/MemberRole.java
@@ -0,0 +1,5 @@
+package org.zerock.b02.domain;
+
+public enum MemberRole {
+    USER, ADMIN;  //유저, 관리자 권한
+}
