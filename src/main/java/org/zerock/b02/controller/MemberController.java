8346f82eae4b93d65b18fd94313a39ba0df586a9
JBKIM <drv98@naver.com>
2024-07-22 10:26:56 +0900

    멤버 조인 컨트롤러

--- src/main/java/org/zerock/b02/controller/MemberController.java
+++ src/main/java/org/zerock/b02/controller/MemberController.java
@@ -1,10 +1,13 @@
 package org.zerock.b02.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.b02.dto.MemberJoinDTO;
 
 @Controller
 @RequestMapping("/member")
@@ -20,5 +23,20 @@ public class MemberController {
         if(logout != null){
             log.info("유저 로그아웃....");
         }
+    }
+
+    //회원가입 페이지를 보여주기
+    @GetMapping("/join")
+    public void joinGET() {
+        log.info("join GET .....");
+    }
+
+    //회원가입 요청
+    @PostMapping("/join")
+    public String joinPOST(MemberJoinDTO memberJoinDTO) {
+        log.info("join POST .....");
+        log.info(memberJoinDTO.toString()); //입력한 멤버정보를 출력
+
+        return "redirect:/board/list"; //회원 가입후 리스트로 이동
     }
 }
